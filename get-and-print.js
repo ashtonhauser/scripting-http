21b5d43cdb7f71d998f2df976b8b864ad51cbdf5
Ashton Hauser <ashton.hauser777@gmail.com>
2018-10-29 18:51:53 +0000

    Actually added get-and-print.js

---
+++ get-and-print.js
@@ -0,0 +1,21 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+  var data = '';
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.on('data', function (chunk) {
+      console.log("im  here");
+      data += chunk;
+    });
+    response.on('end', function () {
+      console.log(data);
+    });
+  });
+}
+
+getAndPrintHTML();
